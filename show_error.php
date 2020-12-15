f012c0cd8d3b656515169e11e8b6039dd1d9a8ed
Tracy Miles <75818815+twmiles@users.noreply.github.com>
2020-12-14 22:13:47 -0500

    Update show_error.php

--- show_error.php
+++ show_error.php
@@ -1,11 +1,7 @@
 <?php
 
-/*
- * Author: Louie Zhu
- * Date: Jan 22, 2016
- * File: show_error.php
- * Description: this script displays an error message.
- */
+//Description: this script displays an error message.
+ 
 require_once 'classes/show_error.class.php';
 
 //retrieve the error message from a querystring variable
